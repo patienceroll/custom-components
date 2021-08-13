232a1027f876c0550e5c8dd0a0360da2f9ad7ed2
patienceroll <1079105171@qq.com>
2021-08-13 16:39:00 +0800

    feat: 🎸 添加打包的时候移动代码到lib

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "rollup";
 
 import rollupPluginTypescript from "rollup-plugin-typescript";
 import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy";
+
+/** @var {"debugge" | 'build'} buildType  */
+const buildType = process.env.type;
 
 const output = {
   debugge: "./debugge",
@@ -10,12 +14,23 @@ const output = {
 
 export default defineConfig({
   input: ["index.ts"],
-  output: [{ dir: output[process.env.type] }],
+  output: [{ dir: output[buildType] }],
   plugins: [
     rollupPluginTypescript({ tsconfig: "tsconfig.json" }),
     postcss({
       extract: true,
       extract: "css/index.css",
+    }),
+    copy({
+      targets: {
+        debugge: [],
+        build: [
+          { src: "components/**/*", dest: "dist/lib/components" },
+          { src: "css/**/*", dest: "dist/lib/css" },
+          { src: "index.ts", dest: "dist/lib" },
+          { src: "package.json", dest: "dist/lib" },
+        ],
+      }[buildType],
     }),
   ],
   preserveModules: true,
