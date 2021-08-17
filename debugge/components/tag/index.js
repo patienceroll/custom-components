b21d9b6015a333201f554e0e7d0c869e31571798
patienceroll <1079105171@qq.com>
2021-08-17 14:26:47 +0800

    refactor: 💡 修复Tag样式问题

--- debugge/components/tag/index.js
+++ debugge/components/tag/index.js
@@ -19,6 +19,7 @@ class Tag extends HTMLElement {
         const textSlot = document.createElement("slot");
         text.setAttribute("part", "text");
         text.appendChild(textSlot);
+        // 创建关闭按钮
         const closeIcon = Tag.initCloseIcon.call(this);
         this.closeIcon = closeIcon;
         shadowRoot.appendChild(icon);
@@ -33,7 +34,7 @@ class Tag extends HTMLElement {
     static initCloseIcon() {
         const closeIcon = document.createElement("span");
         closeIcon.setAttribute("part", "close-icon");
-        closeIcon.innerHTML = `<svg style="font-size:10px" width="1em" height="1em" viewBox="64 64 896 896" focusable="false" data-icon="close" fill="currentColor" aria-hidden="true" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z">
+        closeIcon.innerHTML = `<svg style="font-size:12px" width="1em" height="1em" viewBox="64 64 896 896" focusable="false" data-icon="close" fill="currentColor" aria-hidden="true" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z">
                               </path>
                             </svg>`;
         closeIcon.style.display = "none";
@@ -94,7 +95,7 @@ class Tag extends HTMLElement {
      */
     static setCloseable(closable) {
         if (closable === "true") {
-            this.closeIcon.style.display = "unset";
+            this.closeIcon.style.display = "inline";
         }
         else {
             this.closeIcon.style.display = "none";
