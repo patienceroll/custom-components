a487943312476af1f31a56731a04b8ca0d54e412
zhangxianlei <zhangxianlei@uphicoo.com>
2021-08-18 11:00:51 +0800

    feat: 🎸 涟漪效果coding

--- components/ripple/index.ts
+++ components/ripple/index.ts
@@ -7,8 +7,32 @@ class Ripple extends HTMLElement {
   }
 
   /** 开始涟漪动画 */
-  startPiple() {
-    const rippleItem = document.createElement("span");
+  get startPiple() {
+    return function (
+      this: Ripple,
+      options: {
+        /** 涟漪相对父级起始点 top */
+        top: number;
+        /** 涟漪相对父级起始点 left */
+        left: number;
+        /** 父级宽度 */
+        parentWidth: number;
+        /** 父级高度 */
+        parentHeight: number;
+      }
+    ) {
+      const { pow, sqrt } = Math;
+      const { top, left, parentWidth, parentHeight } = options;
+      const radius = sqrt(pow(parentWidth, 2) + pow(parentHeight, 2));
+      const rippleItem = document.createElement("div");
+      rippleItem.setAttribute("class", "cp-ripple-item");
+      rippleItem.style.top = `${top - radius}px`;
+      rippleItem.style.left = `${left - radius}px`;
+      rippleItem.style.width = `${2 * radius}px`;
+      rippleItem.style.height = `${2 * radius}px`;
+      this.appendChild(rippleItem);
+      return rippleItem;
+    };
   }
 }
 
--- debugge/components/ripple/index.js
+++ debugge/components/ripple/index.js
@@ -3,6 +3,22 @@ class Ripple extends HTMLElement {
     constructor() {
         super();
     }
+    /** 开始涟漪动画 */
+    get startPiple() {
+        return function (options) {
+            const { pow, sqrt } = Math;
+            const { top, left, parentWidth, parentHeight } = options;
+            const radius = sqrt(pow(parentWidth, 2) + pow(parentHeight, 2));
+            const rippleItem = document.createElement("div");
+            rippleItem.setAttribute("class", "cp-ripple-item");
+            rippleItem.style.top = `${top - radius}px`;
+            rippleItem.style.left = `${left - radius}px`;
+            rippleItem.style.width = `${2 * radius}px`;
+            rippleItem.style.height = `${2 * radius}px`;
+            this.appendChild(rippleItem);
+            return rippleItem;
+        };
+    }
 }
 if (!customElements.get("cp-ripple"))
     customElements.define("cp-ripple", Ripple);
