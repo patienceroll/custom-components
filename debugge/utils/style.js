942c07b072e2fc8ae3fa8f0efd41a363090afad2
patienceroll <1079105171@qq.com>
2021-09-10 17:43:01 +0800

    docs: ✏️ 删除没用文件

--- debugge/utils/style.js
+++
@@ -1,8 +0,0 @@
-/** 给html元素添加行内样式 */
-const elementAddStyles = (element, style) => {
-    Object.keys(style).forEach((key) => {
-        element.style.setProperty(key, style[key] || null);
-    });
-};
-
-export { elementAddStyles };
