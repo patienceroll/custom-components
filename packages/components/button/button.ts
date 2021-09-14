4f6e80dcb9ab6099841767641b8e9938c7786ad3
patienceroll <1079105171@qq.com>
2021-09-14 18:05:29 +0800

    docs: ✏️ doc

--- packages/components/button/button.ts
+++ packages/components/button/button.ts
@@ -114,7 +114,8 @@ export default class CpButton extends HTMLElement {
             );
           }
         }
-
+        break;
+      case "loading":
         break;
       default:
         break;
--- packages/components/button/data.d.ts
+++ packages/components/button/data.d.ts
@@ -1,2 +1,2 @@
 /** button组件监听的属性变化的值 */
-export type ButtonObservedAttributes = "disable";
+export type ButtonObservedAttributes = "disable" | "loading";
