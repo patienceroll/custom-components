e677e16a87cfb5aeaa151e315ffdb9ca0e8d6b91
patienceroll <1079105171@qq.com>
2021-09-17 11:32:55 +0800

    docs: ✏️ 删除无用的工具函数

--- packages/utils/style.ts
+++
@@ -1,8 +0,0 @@
-import type { CSSproperty } from "../css/css";
-
-/** 给html元素添加行内样式 */
-export const elementAddStyles = (element: HTMLElement, style: CSSproperty) => {
-  Object.keys(style).forEach((key) => {
-    element.style.setProperty(key, style[key as keyof CSSproperty] || null);
-  });
-};
