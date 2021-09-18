6f0b1d7ff7baa227dff5b9d5c7a62d3e776542d1
patienceroll <1079105171@qq.com>
2021-09-18 09:46:20 +0800

    docs: ✏️ doc

--- packages/components/paper/paper.ts
+++ packages/components/paper/paper.ts
@@ -1,6 +1,12 @@
 
+//  getPropertyPriority(property: string): string;
+//  getPropertyValue(property: string): string;
+//  item(index: number): string;
+//  removeProperty(property: string): string;
+//  setProperty(property: string, value: string | null,
+const createStyle = (params: CssProperty) => {
 
-
+}
 
 export default class CpPaper extends HTMLElement {
   static CpPaperStyleSheet = (() => {
@@ -12,7 +18,6 @@ export default class CpPaper extends HTMLElement {
       box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
       transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
     }`)
-    
     return styleSheet;
   })()
   constructor() {
--- packages/global.d.ts
+++ packages/global.d.ts
@@ -3,6 +3,9 @@ declare interface ShadowRoot {
   adoptedStyleSheets: CSSStyleSheet[];
 }
 
+/** css 属性对象 */
+declare type CssProperty = Partial<Omit<CSSStyleDeclaration, number | 'getPropertyPriority' | 'getPropertyValue' | 'item' | 'removeProperty' | "setProperty">>
+
 
 /** 开启了shadowRoot的元素 */
 declare type AttachedShadowRoot<T> = Omit<T, 'shadowRoot'> & {
