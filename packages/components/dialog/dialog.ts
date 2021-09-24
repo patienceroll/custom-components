36aa79e627378369ce49bb717e74228c040c9b2c
李清龙 <924229679@qq.com>
2021-09-24 18:13:38 +0800

    feat(mask&dialog): 完善这俩个组件

--- packages/components/dialog/dialog.ts
+++ packages/components/dialog/dialog.ts
@@ -1,76 +1,35 @@
 import { formatStyle, formatKeyframes, setDomNodeStyle } from '../../utils/style';
 import CpMask from '../mask/mask';
 import { DialogType } from './data';
-export default class CpDialog extends CpMask {
+
+export default class CpDialog extends CpMask implements CustomElement {
 	content: HTMLElement;
 	type: DialogType = 'modal';
 	static styleSheet: CSSStyleSheet;
 	static keyframesSheet: CSSStyleSheet;
+	static style: CSSStyleObject;
+	static keyframes: KeyframeObject;
 	// 基础层级
 	static baseIndex = 0;
 	// 鼠标位置
 	mousePosition = { x: '', y: '' };
 
-	static keyframes: KeyframeObject = {
-		show: {
-			'0%': {
-				opacity: '0',
-			},
-			'100%': {
-				opacity: '1',
-			},
-		},
-		hiden: {
-			'0%': {
-				opacity: '1',
-			},
-			'100%': {
-				opacity: '0',
-			},
-		},
-	};
-
-	static style: CSSStyleObject = {
-		'.cp-dialog-hiden': {
-			opacity: '0',
-			animation: 'hiden 0.3s',
-		},
-		'.cp-dialog-show': {
-			opacity: '1',
-			animation: 'show 0.3s',
-		},
-		'.cp-dialog-modal-content': {
-			position: 'fixed',
-			top: '20%',
-			fontSize: '40px',
-			left: '50%',
-			transform: 'translateX(-50%)',
-			transition: 'all .2s ease',
-			zIndex: '100',
-		},
-		'.cp-dialog-drawer-content': {
-			position: 'fixed',
-			top: '0',
-			fontSize: '40px',
-			right: '0',
-			transition: 'all .2s ease',
-			minWidth: '500px',
-			height: '100vh',
-			backgroundColor: '#fff',
-			boxShadow: '-5px 0px 15px rgba(0,0,0,0.2)',
-			zIndex: '100',
-		},
-	};
-
 	constructor() {
-		super([CpDialog.styleSheet, CpDialog.keyframesSheet]);
+		super();
 
 		const content = document.createElement('div');
 		const contentSlot = document.createElement('slot');
 		content.append(contentSlot);
 		this.content = content;
 		this.setDialogClass();
-		this.shadowRoot?.append(content);
+		if (this.shadowRoot) {
+			this.shadowRoot?.append(content);
+			this.shadowRoot.adoptedStyleSheets = [
+				...this.shadowRoot.adoptedStyleSheets,
+				CpDialog.styleSheet,
+				CpDialog.keyframesSheet,
+			];
+		}
 	}
 
 	private setDialogClass() {
@@ -111,7 +70,7 @@ export default class CpDialog extends CpMask {
 						if (this.content) {
 							setDomNodeStyle(this.content, { left: '', top: '', opacity: '1' });
 						}
-					}, 200);
+					}, 300);
 				} else {
 					setDomNodeStyle(this.content, { left: `${x}px`, top: `${y}px`, opacity: '0' });
 				}
@@ -122,7 +81,7 @@ export default class CpDialog extends CpMask {
 						if (this.content) {
 							setDomNodeStyle(this.content, { right: '', top: '', opacity: '1' });
 						}
-					}, 200);
+					}, 300);
 				} else {
 					setDomNodeStyle(this.content, { right: '-100%', top: '0', opacity: '0' });
 				}
@@ -130,14 +89,71 @@ export default class CpDialog extends CpMask {
 		}
 	}
 
-	closeCallback() {
+	onBeforeClose() {
 		this.setmousePosition(false);
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve('close');
+			}, 200);
+		});
 	}
 
-	showCallback() {
+	onBeforeShow() {
 		this.setmousePosition();
 	}
 }
+
+CpDialog.keyframes = {
+	show: {
+		'0%': {
+			opacity: '0',
+		},
+		'100%': {
+			opacity: '1',
+		},
+	},
+	hiden: {
+		'0%': {
+			opacity: '1',
+		},
+		'100%': {
+			opacity: '0',
+		},
+	},
+};
+
+CpDialog.style = {
+	'.cp-dialog-hiden': {
+		opacity: '0',
+		animation: 'hiden 0.3s',
+	},
+	'.cp-dialog-show': {
+		opacity: '1',
+		animation: 'show 0.3s',
+	},
+	'.cp-dialog-modal-content': {
+		position: 'absolute',
+		top: '20%',
+		fontSize: '40px',
+		left: '50%',
+		transform: 'translateX(-50%)',
+		transition: 'all .3s ease',
+		zIndex: '100',
+	},
+	'.cp-dialog-drawer-content': {
+		position: 'fixed',
+		top: '0',
+		fontSize: '40px',
+		right: '0',
+		transition: 'all .3s ease',
+		minWidth: '500px',
+		height: '100vh',
+		backgroundColor: '#fff',
+		boxShadow: '-5px 0px 15px rgba(0,0,0,0.2)',
+		zIndex: '100',
+		overflow: 'auto',
+	},
+};
 
 CpDialog.styleSheet = formatStyle(CpDialog.style);
 CpDialog.keyframesSheet = formatKeyframes(CpDialog.keyframes);
