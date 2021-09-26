9db89d32a8f472143d19708fcd46da07f00524aa
patienceroll <1079105171@qq.com>
2021-09-26 17:42:40 +0800

    feat: 🎸 coding 单选框组件

--- packages/components/radio/radio.ts
+++ packages/components/radio/radio.ts
@@ -1,6 +1,23 @@
+import { formatStyle } from 'packages/utils/style';
+
 export default class CpRadio extends HTMLElement implements CustomElement {
+	#styleSheet?: CSSStyleSheet;
+	#style: CSSStyleObject = {
+		':host': {},
+	};
+
 	constructor() {
 		super();
-		const shadowRoot = this.attachShadow({ mode: "open" });
+		const shadowRoot = this.attachShadow({ mode: 'open' });
+		if (this.#styleSheet === undefined) this.#styleSheet = formatStyle(this.#style);
+		shadowRoot.adoptedStyleSheets = [this.#styleSheet];
+
+		const radio = document.createElement('input');
+		radio.type = 'radio';
+		const label = document.createElement('label');
+		const labelText = document.createElement('slot');
+
+		label.append(radio, labelText);
+		shadowRoot.append(label);
 	}
 }
