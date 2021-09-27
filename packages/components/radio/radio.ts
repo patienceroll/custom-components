a4f663b5743026a19275182f1a273a7b97041a45
patienceroll <1079105171@qq.com>
2021-09-27 17:58:32 +0800

    feat: 🎸 coding radio

--- packages/components/radio/radio.ts
+++ packages/components/radio/radio.ts
@@ -1,13 +1,47 @@
 import type { CpRadioObservedAttributes } from './data';
+import type Ripple from '../ripple/ripple';
 
 import { formatStyle } from 'packages/utils/style';
+
+import '../ripple';
 
 export default class CpRadio extends HTMLElement implements CustomElement {
 	/** 组件实例 radio 元素 */
 	radio: HTMLInputElement;
 	#styleSheet?: CSSStyleSheet;
 	#style: CSSStyleObject = {
-		':host': {},
+		'.text-slot': {
+			display: 'inline-block',
+			lineHeight: '1.2em',
+			verticalAlign: 'middle',
+		},
+		'.cp-radio-radio-wrapper > input': {
+			margin: '0',
+			width: '0.6em',
+			height: '0.6em',
+			left: '0.6em',
+			top: '0.6em',
+			position: 'absolute',
+		},
+		'.cp-radio-radio-wrapper:hover': {
+			backgroundColor: '#e0e0e0',
+		},
+		'.cp-radio-radio-wrapper': {
+			display: 'inline-block',
+			position: 'relative',
+			width: '1.2em',
+			height: '1.2em',
+			padding: '0.6em',
+			borderRadius: '50%',
+			verticalAlign: 'middle',
+			overflow: 'hidden',
+		},
+		'.cp-radio': {
+			display: 'inline-block',
+		},
+		':host': {
+			display: 'inline-block',
+		},
 	};
 
 	constructor() {
@@ -16,9 +50,11 @@ export default class CpRadio extends HTMLElement implements CustomElement {
 		if (this.#styleSheet === undefined) this.#styleSheet = formatStyle(this.#style);
 		shadowRoot.adoptedStyleSheets = [this.#styleSheet];
 
-		const radio = document.createElement('input');
 		const label = document.createElement('label');
-		const labelText = document.createElement('slot');
+		const radioWrapper = document.createElement('span');
+		const radio = document.createElement('input');
+		const text = document.createElement('slot');
+		const ripple = document.createElement('cp-ripple') as AttachedShadowRoot<Ripple>;
 
 		this.radio = radio;
 		radio.type = 'radio';
@@ -27,7 +63,22 @@ export default class CpRadio extends HTMLElement implements CustomElement {
 			this.setAttribute('checked', radio.checked ? 'true' : 'false');
 		});
 
-		label.append(radio, labelText);
+		label.classList.add('cp-radio');
+		radioWrapper.classList.add('cp-radio-radio-wrapper');
+		text.classList.add('text-slot');
+
+		radioWrapper.addEventListener('click', (event) => {
+			event.preventDefault();
+			ripple
+				.start({
+					top: radioWrapper.clientHeight / 2,
+					left: radioWrapper.clientWidth / 2,
+				})
+				.then(({ stop }) => stop());
+		});
+
+		radioWrapper.append(ripple, radio);
+		label.append(radioWrapper, text);
 		shadowRoot.append(label);
 	}
 
