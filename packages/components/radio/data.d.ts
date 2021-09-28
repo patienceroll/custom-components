22f32173671a8bb413b3323292a304f550873144
patienceroll <1079105171@qq.com>
2021-09-28 17:58:35 +0800

    docs: ✏️ doc

--- packages/components/radio/data.d.ts
+++ packages/components/radio/data.d.ts
@@ -1,8 +1,11 @@
-export type CpRadioObservedAttributes = 'checked';
+export type CpRadioObservedAttributes = 'checked' | 'name' | 'color';
 
 export type CpRadioProps = {
 	/** 是否选中 */
 	'checked'?: BooleanCharacter;
 	/** 是否默认选中 */
 	'default-checked'?: BooleanCharacter;
+	'name'?: string;
+	/** 单选 icon 的颜色 */
+	'color'?: string;
 };
--- packages/components/radio/index.ts
+++ packages/components/radio/index.ts
@@ -1,5 +1,9 @@
 import CpRadio from './radio';
+import CpRadioGroup from './radio-gropu';
 
 if (customElements.get('cp-radio') === undefined) {
 	customElements.define('cp-radio', CpRadio);
+}
+if (customElements.get('cp-radio-group') === undefined) {
+	customElements.define('cp-radio-group', CpRadioGroup);
 }
