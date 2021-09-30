719a196943566d534526f01de929739e006c65be
patienceroll <1079105171@qq.com>
2021-09-30 17:11:24 +0800

    docs: ✏️ doc

--- packages/utils/decorators.ts
+++ packages/utils/decorators.ts
@@ -1,11 +1,13 @@
 import { formatKeyframes, formatStyle } from '../utils/style';
 
+/** 自定义组件样式装饰器 */
 export function style(parma: CSSStyleObject) {
 	return function styledecorators(target: any) {
 		target.styleSheet = formatStyle(parma);
 	};
 }
 
+/** 自定义组件动画装饰器 */
 export function keyframe(parma: KeyframeObject) {
 	return function styledecorators(target: any) {
 		target.keyframesSheet = formatKeyframes(parma);
