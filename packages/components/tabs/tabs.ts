c57a93c714e0533cc06903d7df3b3b8b44093ec9
patienceroll <1079105171@qq.com>
2021-11-18 10:39:28 +0800

    feat: 🎸 coding tab

--- packages/components/tabs/tabs.ts
+++ packages/components/tabs/tabs.ts
@@ -10,12 +10,12 @@ import CpTab from './tab';
 	},
 	'.cp-tabs-weapper-bar': {
 		position: 'absolute',
-		width: '74px',
+		width: '0',
 		bottom: '0',
 		left: '0',
 		height: '2px',
 		backgroundColor: '#007FFF',
-		transition: 'left ease 300ms,width ease 300ms',
+		transition: 'left ease 500ms,width ease 300ms',
 	},
 	'.cp-tabs-wrapper': {
 		display: 'flex',
@@ -27,13 +27,14 @@ import CpTab from './tab';
 		borderBottom: '1px solid rgba(0,0,0,0.12)',
 	},
 })
-@watch<CpTabsObservedAttributes, AttachedShadowRoot<CpTabs>>(['center'], function (attr, older, newer) {
+@watch<CpTabsObservedAttributes, AttachedShadowRoot<CpTabs>>(['center', 'active-key'], function (attr, older, newer) {
 	switch (attr) {
 		case 'center':
 			if (newer === 'true') this.wrapper.style.justifyContent = 'center';
 			else this.wrapper.style.removeProperty('justifyContent');
 			break;
 		case 'active-key':
+			if (newer) this.setRealActiveKey(newer);
 			break;
 	}
 })
@@ -62,7 +63,7 @@ export default class CpTabs extends HTMLElement implements CustomElement {
 
 		this.addEventListener('cp-tab-click', (event) => {
 			const { detail } = event as CustomEvent<{ domEvent: MouseEvent; key: string | null }>;
-			if (detail.key) this.activeTab(detail.key);
+			if (detail.key) this.setRealActiveKey(detail.key);
 		});
 
 		this.wrapper.append(children, this.wrapperBar);
@@ -74,15 +75,30 @@ export default class CpTabs extends HTMLElement implements CustomElement {
 		return Array.from(this.children).filter((node) => node.localName === 'cp-tab') as CpTab[];
 	}
 
-	/** 生成tab选项 */
-	generateTab() {}
-
 	/** 激活某一个tab,其余tab应该被取消激活 */
-	activeTab(tabKey: string) {
+	activeTab(activeKey: string) {
 		this.tabNodes.forEach((tab) => {
-			if (tab.key === tabKey) tab.active();
-			else tab.cancelAtive();
+			if (tab.key === activeKey) {
+				tab.active();
+			} else tab.cancelAtive();
 		});
+	}
+
+	/** 渲染tab-bar */
+	renderBar(activeKey: string) {
+		const tab = this.tabNodes.find((t) => t.key === activeKey);
+		if (tab) {
+			// tab 的父级是slot,但是偏移的父级是 slot 的父级,这里为什么还没确定
+			this.wrapperBar.style.left = `${tab.offsetLeft}px`;
+			this.wrapperBar.style.width = `${tab.clientWidth}px`;
+		}
+	}
+
+	/** 设置保存在组件内部的 activeKey */
+	setRealActiveKey(activeKey: string) {
+		this.realActiveKey = activeKey;
+		this.activeTab(activeKey);
+		this.renderBar(activeKey);
 	}
 
 	connectedCallback() {}
